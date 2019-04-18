08ffdfbd20b783db33a8d3e423cfdaf58f8c2e27
Loreto Maureira <lmaureirav@yahoo.com>
2019-04-17 22:45:52 -0400

    Feat:comentario navegacion

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -313,54 +313,22 @@ cursosApp.controller('menuInteriorCrtl', ['$scope', '$location', function ($scop
     }
   }]);
   
-cursosApp.controller('menuPrivadoCrtl-p', ['$scope', 'screenSize', '$location', function ($scope, screenSize) {
+cursosApp.controller('menuPrivadoCrtl-p', ['$scope', '$location', function ($scope, screenSize) {
   $scope.activo = [];
   $scope.visible = [];
 
-  $scope.isMobile = screenSize.on('xs, sm', function (isMatch) {
-    $scope.isMobile = isMatch;
-  });
-
-  $scope.menuAbierto = false;
-  $scope.subMenuActivo = -1;
-
-  $scope.mouseOver = function (position) {
-    $scope.activo[position] = "activo-menuPrivado";
-    $scope.visible[position] = true;
-  };
-
-  $scope.mouseLeave = function (position) {
-    $scope.activo[position] = "";
-    $scope.visible[position] = false;
-
-  };
-  $scope.mouseClick = function (position) {
-    $scope.activo[position] = "";
-    $scope.visible[position] = false;
-  };
-  $scope.mouseClick = function (position) {
-    $scope.activo[position] = "";
-    $scope.visible[position] = true;
-  };
-  $scope.nextPage = function () {
-    var elements = angular.element(document.querySelector('#menuPrivadoCategoria'));
-    console.log(elements.prop('offsetTop'));
-    elements.top = elements.prop('offsetTop') + 30;
-  };
+  
   $scope.isActive = false;
   $scope.activeButton = function (menuId) {
     $scope.currentOpen.menuId = menuId;
   };
 
-  $scope.isActiveMenu = function (destination) {
-    return destination === $scope.path();
-  }
 
   $scope.currentOpen = {
     menuId: null
   };
 
-
+// inicio nav superior
   $scope.menuEmpresa = [{
     id: 1,
     name: 'BOLETA DE GARANTÍA',
@@ -402,7 +370,9 @@ cursosApp.controller('menuPrivadoCrtl-p', ['$scope', 'screenSize', '$location', 
       subMenu: []
     }]
   }];
+// fin nav superior
 
+// inicio nav flotante izquierdo
   $scope.menuEmpresa4 = [{
     id: 4,
     name: 'FLUJO DEL PROCESO COMERCIAL',
@@ -417,29 +387,5 @@ cursosApp.controller('menuPrivadoCrtl-p', ['$scope', 'screenSize', '$location', 
     }]
   }];
 
-  $scope.mostrarOcultarSubMenu = function (indice, longitudMenu) {
-    if ($scope.subMenuActivo == -1 || $scope.subMenuActivo == indice) {
-      $scope.menuAbierto = !$scope.menuAbierto;
-    }
-    $scope.subMenuActivo = $scope.menuAbierto ? indice : -1;
-    if (longitudMenu == 0) {
-      $scope.cerrarMenu();
-    }
-  }
-
-  $scope.cerrarMenu = function () {
-    $scope.currentOpen.menuId = false;
-    $scope.isActive = null;
-    $scope.subMenuActivo = -1;
-  }
-
-
-  function previousPage() {
-    var i, tab, elements = getElements();
-    for (i = 0; i < elements.tabs.length; i++) {
-      tab = elements.tabs[i];
-      if (tab.offsetLeft + tab.offsetWidth >= ctrl.offsetLeft) break;
-    }
-    ctrl.offsetLeft = fixOffset(tab.offsetLeft + tab.offsetWidth - elements.canvas.clientWidth);
-  }
+// fin nav flotante izquierdo
 }]);
